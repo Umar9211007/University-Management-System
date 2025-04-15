01e76e0fb023ffc5255b263b4b1704ea352e4922
Umar9211007 <157800837+Umar9211007@users.noreply.github.com>
2025-04-15 21:35:43 +0500

    code delete do dafa used hoya ta

--- index.js
+++ index.js
@@ -61,10 +61,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(...));
-  app.get('*', ...);
-}
